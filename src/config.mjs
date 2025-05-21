2dfeda5e7b9c96e2a1f6bd37208986d1f1751823
Thelma <141613467+thelssa@users.noreply.github.com>
2025-05-21 09:53:18 +0200

    Update config.mjs

--- src/config.mjs
+++ src/config.mjs
@@ -2,11 +2,11 @@ export default {
   development: {
     type: 'development',
     port: 3000,
-    mongodb: 'mongodb+srv://username:MDP@efrei-m1.vktvy.mongodb.net/ecole'
+    mongodb: 'mongodb+srv://username:MDP@....mongodb.net/db' //remplacer par le votre
   },
   production: {
     type: 'production',
     port: 3000,
-    mongodb: 'mongodb+srv://username:MDP@efrei-m1.vktvy.mongodb.net/ecole'
+    mongodb: 'mongodb+srv://username:MDP@....mongodb.net/db'
   }
 };
